c2e07c61a96bf2d136b078153259eea1ba72319f
pedrosimil <149528572+pedrosimil@users.noreply.github.com>
2025-07-10 05:14:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -75,6 +75,14 @@
         </div>
     </div>
 
+    <footer class="mt-5 p-4 bg-light text-center text-muted">
+    <div class="container">
+        <p class="mb-0">
+            Desenvolvido por Pedro Simil - <?php echo date("Y"); ?>
+        </p>
+    </div>
+</footer>
+
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
